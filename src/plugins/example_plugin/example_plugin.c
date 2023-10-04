f51b7be7e6db0eda845a4e99415d1bb363f573f1
Stephan Bauer <st.bauer04@gmail.com>
2023-10-04 17:18:40 +0200

    Step 1: Hello world!

--- src/plugins/example_plugin/example_plugin.c
+++ src/plugins/example_plugin/example_plugin.c
@@ -15,153 +15,37 @@
  * limitations under the License.
  */
 
-#include <vnet/vnet.h>
 #include <vnet/plugin/plugin.h>
 #include <example_plugin/example_plugin.h>
 
-#include <vlibapi/api.h>
-#include <vlibmemory/api.h>
 #include <vpp/app/version.h>
-#include <stdbool.h>
-
-#include <example_plugin/example_plugin.api_enum.h>
-#include <example_plugin/example_plugin.api_types.h>
-
-#define REPLY_MSG_ID_BASE emp->msg_id_base
-#include <vlibapi/api_helper_macros.h>
 
 example_plugin_main_t example_plugin_main;
 
 /* Action function shared between message handler and debug CLI */
 
-int example_plugin_enable_disable (example_plugin_main_t * emp, u32 sw_if_index,
-                                   int enable_disable)
-{
-  vnet_sw_interface_t * sw;
-  int rv = 0;
-
-  /* Utterly wrong? */
-  if (pool_is_free_index (emp->vnet_main->interface_main.sw_interfaces,
-                          sw_if_index))
-    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
-
-  /* Not a physical port? */
-  sw = vnet_get_sw_interface (emp->vnet_main, sw_if_index);
-  if (sw->type != VNET_SW_INTERFACE_TYPE_HARDWARE)
-    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
-
-  example_plugin_create_periodic_process (emp);
-
-  vnet_feature_enable_disable ("device-input", "example_plugin",
-                               sw_if_index, enable_disable, 0, 0);
-
-  /* Send an event to enable/disable the periodic scanner process */
-  vlib_process_signal_event (emp->vlib_main,
-                             emp->periodic_node_index,
-                             EXAMPLE_PLUGIN_EVENT_PERIODIC_ENABLE_DISABLE,
-                            (uword)enable_disable);
-  return rv;
-}
-
 static clib_error_t *
-example_plugin_enable_disable_command_fn (vlib_main_t * vm,
+example_plugin_command_fn (vlib_main_t * vm,
                                    unformat_input_t * input,
                                    vlib_cli_command_t * cmd)
 {
-  example_plugin_main_t * emp = &example_plugin_main;
-  u32 sw_if_index = ~0;
-  int enable_disable = 1;
+  if (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+      return clib_error_return (0, "Syntax error");
 
-  int rv;
+  vlib_cli_output (vm, "Hello world!");
 
-  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
-    {
-      if (unformat (input, "disable"))
-        enable_disable = 0;
-      else if (unformat (input, "%U", unformat_vnet_sw_interface,
-                         emp->vnet_main, &sw_if_index))
-        ;
-      else
-        break;
-  }
-
-  if (sw_if_index == ~0)
-    return clib_error_return (0, "Please specify an interface...");
-
-  rv = example_plugin_enable_disable (emp, sw_if_index, enable_disable);
-
-  switch(rv)
-    {
-  case 0:
-    break;
-
-  case VNET_API_ERROR_INVALID_SW_IF_INDEX:
-    return clib_error_return
-      (0, "Invalid interface, only works on physical ports");
-    break;
-
-  case VNET_API_ERROR_UNIMPLEMENTED:
-    return clib_error_return (0, "Device driver doesn't support redirection");
-    break;
-
-  default:
-    return clib_error_return (0, "example_plugin_enable_disable returned %d",
-                              rv);
-    }
   return 0;
 }
 
 /* *INDENT-OFF* */
 VLIB_CLI_COMMAND (example_plugin_enable_disable_command, static) =
 {
-  .path = "example_plugin enable-disable",
+  .path = "example_plugin cmd",
   .short_help =
-  "example_plugin enable-disable <interface-name> [disable]",
-  .function = example_plugin_enable_disable_command_fn,
+  "example_plugin cmd <string>",
+  .function = example_plugin_command_fn,
 };
 /* *INDENT-ON* */
-
-/* API message handler */
-static void vl_api_example_plugin_enable_disable_t_handler
-(vl_api_example_plugin_enable_disable_t * mp)
-{
-  vl_api_example_plugin_enable_disable_reply_t * rmp;
-  example_plugin_main_t * emp = &example_plugin_main;
-  int rv;
-
-  rv = example_plugin_enable_disable (emp, ntohl(mp->sw_if_index),
-                                      (int) (mp->enable_disable));
-
-  REPLY_MACRO(VL_API_EXAMPLE_PLUGIN_ENABLE_DISABLE_REPLY);
-}
-
-/* API definitions */
-#include <example_plugin/example_plugin.api.c>
-
-static clib_error_t * example_plugin_init (vlib_main_t * vm)
-{
-  example_plugin_main_t * emp = &example_plugin_main;
-  clib_error_t * error = 0;
-
-  emp->vlib_main = vm;
-  emp->vnet_main = vnet_get_main();
-
-  /* Add our API messages to the global name_crc hash table */
-  emp->msg_id_base = setup_message_id_table ();
-
-  return error;
-}
-
-VLIB_INIT_FUNCTION (example_plugin_init);
-
-/* *INDENT-OFF* */
-VNET_FEATURE_INIT (example_plugin, static) =
-{
-  .arc_name = "device-input",
-  .node_name = "example_plugin",
-  .runs_before = VNET_FEATURES ("ethernet-input"),
-};
-/* *INDENT-ON */
 
 /* *INDENT-OFF* */
 VLIB_PLUGIN_REGISTER () =
--- src/plugins/example_plugin/example_plugin.h
+++ src/plugins/example_plugin/example_plugin.h
@@ -19,38 +19,17 @@
 #define __included_example_plugin_h__
 
 #include <vnet/vnet.h>
-#include <vnet/ip/ip.h>
-#include <vnet/ethernet/ethernet.h>
-
-#include <vppinfra/hash.h>
-#include <vppinfra/error.h>
 
 typedef struct {
     /* API message ID base */
     u16 msg_id_base;
 
-    /* on/off switch for the periodic function */
-    u8 periodic_timer_enabled;
-    /* Node index, non-zero if the periodic process has been created */
-    u32 periodic_node_index;
-
     /* convenience */
     vlib_main_t * vlib_main;
     vnet_main_t * vnet_main;
-    ethernet_main_t * ethernet_main;
 } example_plugin_main_t;
 
 extern example_plugin_main_t example_plugin_main;
-
-extern vlib_node_registration_t example_plugin_node;
-extern vlib_node_registration_t example_plugin_periodic_node;
-
-/* Periodic function events */
-#define EXAMPLE_PLUGIN_EVENT1 1
-#define EXAMPLE_PLUGIN_EVENT2 2
-#define EXAMPLE_PLUGIN_EVENT_PERIODIC_ENABLE_DISABLE 3
-
-void example_plugin_create_periodic_process (example_plugin_main_t *);
 
 #endif /* __included_example_plugin_h__ */
 
