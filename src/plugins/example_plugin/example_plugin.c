3b55a5195757621b87a4416bc21e202ac935c984
Stephan Bauer <st.bauer04@gmail.com>
2023-10-05 18:59:44 +0200

    Step 3: Read an address

--- src/plugins/example_plugin/example_plugin.c
+++ src/plugins/example_plugin/example_plugin.c
@@ -18,6 +18,9 @@
 #include <vnet/plugin/plugin.h>
 #include <example_plugin/example_plugin.h>
 
+#include <vnet/ip/ip_format_fns.h>
+#include <vnet/ip/ip4_packet.h>
+
 #include <vpp/app/version.h>
 
 example_plugin_main_t example_plugin_main;
@@ -30,14 +33,24 @@ example_plugin_command_fn (vlib_main_t * vm,
                                    vlib_cli_command_t * cmd)
 {
     u8* str;
+    u8 ip_addr_set = 0;
+    ip4_address_t ip_addr;
+    u8* result;
+
     if (!unformat (input, "%s", &str))
         return clib_error_return (0, "Syntax error");
+
+    if (unformat (input, "address %U", unformat_ip4_address, &ip_addr))
+        ip_addr_set = 1;
 
     if (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
         return clib_error_return (0, "Syntax error");
 
-  vlib_cli_output (vm, "Hello world! (%s)", str);
-  return 0;
+    result = format(0, "Hello world! (%s)", str);
+    if (ip_addr_set)
+        result = format(result, " %U", format_ip4_address, &ip_addr);
+    vlib_cli_output (vm, (char*)result);
+    return 0;
 }
 
 /* *INDENT-OFF* */
@@ -45,7 +58,7 @@ VLIB_CLI_COMMAND (example_plugin_enable_disable_command, static) =
 {
   .path = "example_plugin cmd",
   .short_help =
-  "example_plugin cmd <string>",
+  "example_plugin cmd <string> [address <address>]",
   .function = example_plugin_command_fn,
 };
 /* *INDENT-ON* */
